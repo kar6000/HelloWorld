36f5360fad639d36c29656db468aae0dd0e9bf02
kar6000 <34102279+kar6000@users.noreply.github.com>
2018-01-04 22:28:18 -0800

    Add files via upload

---
+++ DataTypeJava.java
@@ -0,0 +1,54 @@
+package com.corejava.practice;
+
+public class DataTypeJava {
+
+	public static void main(String[] args) {
+
+		// Primitive data types: int, double, char, boolean
+		// Non Primitive Data Types: String, Array
+
+		// 1. int(4 bytes), byte(1 byte),long(8 bytes), short(2 bytes)
+
+		int i = 10;
+		i = 40;
+		int age = 25;
+		int h = -10;
+		int t = 0;
+
+		// 2. double(8 bytes), float(4 bytes)
+
+		double d1 = 12.56;
+		double d2 = -12.89;
+		double d3 = 9;
+
+		// 3.Char
+
+		char c1 = 'a';
+		char c2 = 'b';
+		char c3 = '8';
+		char gender = 'F';
+
+		// 4.boolean
+
+		boolean b1 = true;
+		boolean b2 = false;
+
+		// String: is a class in Java, it is not a datatype
+
+		String s1 = "Ipsita";
+		String s2 = "Hello world";
+		String s3 = "I like Java";
+		String s4 = "10.33";
+		String s5 = "1000";
+
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		System.out.println("Hello Ipsita");
+		System.out.println(c1);
+		System.out.println(c1 + c2);
+		System.out.println(c1 + c2 + c3);
+
+	}
+
+}
